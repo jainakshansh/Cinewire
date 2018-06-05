4e8f328d245a0e993593dae1e9b896706e234b80
Akshansh Jain <akshanshjn9@gmail.com>
2018-06-05 22:41:08 +0530

    Added splash activity

--- app/src/main/java/me/akshanshjain/popularmovies/SplashActivity.java
+++ app/src/main/java/me/akshanshjain/popularmovies/SplashActivity.java
@@ -1,13 +1,41 @@
 package me.akshanshjain.popularmovies;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private TextView appName;
+    private Typeface qBold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Setting up the activity for full screen mode.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        qBold = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Bold.ttf");
+        appName = findViewById(R.id.name_splash);
+        //Setting the font family to text.
+        appName.setTypeface(qBold);
+
+        //Starting new activity after a delay of 1.5 seconds.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 1500);
+
     }
 }
