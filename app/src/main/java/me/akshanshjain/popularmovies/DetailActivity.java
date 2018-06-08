5744e2f9c96d8a5bcf3107c4bdd03e081bcfaec5
Akshansh Jain <akshanshjn9@gmail.com>
2018-06-08 16:52:45 +0530

    Created detailed view layout.

--- app/src/main/java/me/akshanshjain/popularmovies/DetailActivity.java
+++ app/src/main/java/me/akshanshjain/popularmovies/DetailActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private ImageView moviePoster;
+    private TextView movieName, movieOverview, movieRating, movieRelease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,22 @@ public class DetailActivity extends AppCompatActivity {
             }
         }
 
+        //Getting the passed data from the intent.
         String id = intent.getStringExtra("ID");
         String name = intent.getStringExtra("NAME");
         String rating = intent.getStringExtra("VOTE_AVG");
         String image = intent.getStringExtra("IMAGE");
+        String overview = intent.getStringExtra("OVERVIEW");
 
-        Toast.makeText(this, id + "\t" + name + "\t" + rating + "\t" + image, Toast.LENGTH_LONG).show();
+        //Initializing the views from the XML.
+        initViews();
+    }
+
+    private void initViews() {
+        moviePoster = findViewById(R.id.movie_poster_detail);
+        movieName = findViewById(R.id.movie_name_detail);
+        movieOverview = findViewById(R.id.movie_overview_detail);
+        movieRating = findViewById(R.id.movie_rating_detail);
+        movieRelease = findViewById(R.id.movie_release_detail);
     }
 }
