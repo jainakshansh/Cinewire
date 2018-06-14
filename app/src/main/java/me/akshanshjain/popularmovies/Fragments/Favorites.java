7ee8ec7cc063272b5560d15bb7cb03c865c98e94
Akshansh Jain <akshanshjn9@gmail.com>
2018-06-15 00:56:27 +0530

    Added fragments, created and initialized common layout in all

---
+++ app/src/main/java/me/akshanshjain/popularmovies/Fragments/Favorites.java
@@ -0,0 +1,24 @@
+package me.akshanshjain.popularmovies.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.akshanshjain.popularmovies.R;
+
+public class Favorites extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //Setting the layout for the fragment
+        View view = inflater.inflate(R.layout.fragment_common_layout, container, false);
+
+        return view;
+    }
+}
